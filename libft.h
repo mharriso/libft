8501cf58fba83e7401a31c14aa129f4aaee0e0ed
Julia <63446729+mharriso@users.noreply.github.com>
2020-10-10 19:12:53 +0300

    Update libft.h

--- libft.h
+++ libft.h
@@ -22,11 +22,11 @@ void ft_putendl_fd(char *s, int fd);
 void ft_putnbr_fd(int n, int fd);
 void *ft_memcpy(void *dest, const void *source, size_t count);
 void *ft_memccpy(void *dest, const void *source, int ch, size_t count);
-/*int		ft_isascii(int c);
+int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
 int		ft_isalnum(int c);
 int		ft_tolower(int c);
-int		ft_toupper(int c);*/
+int		ft_toupper(int c);
 
 #endif
