e99f10dfc2ff6fca537a63ed3591b9d709db6d54
User <user@MBP-User.Dlink>
2020-10-28 01:23:38 +0300

    upd

--- ft_memchr.c
+++ ft_memchr.c
@@ -3,7 +3,10 @@
 void *ft_memchr(const void *arr, int c, size_t n)
 {
     while(n--)
-        if(*((unsigned char *)arr++) == c)
-            return (void *)arr - 1;
+    {
+        if(*((unsigned char *)arr) == c)
+            return (void *)arr;
+        (unsigned char *)arr++;
+    }
     return (NULL);
 }
--- libft.h
+++ libft.h
@@ -58,6 +58,9 @@ typedef struct		s_list
 }					t_list;
 
 t_list *ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int ft_lstsize(t_list *lst);
+t_list *ft_lstlast(t_list *lst);
 int ft_isspace(int c);
 
 #endif
