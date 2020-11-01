1a60e0a089ff25f46d7a123dbdc2659232a8e6f2
Juliq Kanoda <mharriso@at-l7.msk.21-school.ru>
2020-11-01 20:14:19 +0300

    upd

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mharriso <mharriso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/28 17:58:37 by mharriso          #+#    #+#             */
-/*   Updated: 2020/11/01 00:06:10 by mharriso         ###   ########.fr       */
+/*   Updated: 2020/11/01 20:13:27 by mharriso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t i;
 
 	i = 0;
-	while (s1[i] && s2[i] && i < n)
+	while ((s1[i] || s2[i]) && i < n)
 	{
 		if (s1[i] != s2[i])
 			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 		i++;
 	}
-	if (i < n && (s1[i] || s2[i]))
-		return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 	return (0);
 }
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: mharriso <mharriso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/28 18:31:31 by mharriso          #+#    #+#             */
-/*   Updated: 2020/10/30 19:27:38 by mharriso         ###   ########.fr       */
+/*   Updated: 2020/11/01 19:27:33 by mharriso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	i = 0;
 	while (big[i] && i < len)
 	{
-		if (ft_strncmp(&big[i], little, ft_strlen(little)) == 0)
-			return ((char *)&big[i]);
+		if (ft_strncmp(big + i, little, ft_strlen(little)) == 0)
+			return ((char *)big + i);
 		i++;
 	}
 	return (NULL);
