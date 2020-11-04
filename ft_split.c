6c1c7e49d9d262fb6d5e654cb65aea219e574987
Juliq Kanoda <mharriso@il-i1.msk.21-school.ru>
2020-11-04 04:49:25 +0300

    norme

--- ft_split.c
+++ ft_split.c
@@ -6,86 +6,73 @@
 /*   By: mharriso <mharriso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/28 17:49:39 by mharriso          #+#    #+#             */
-/*   Updated: 2020/11/03 21:57:38 by mharriso         ###   ########.fr       */
+/*   Updated: 2020/11/04 04:45:27 by mharriso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-static  size_t    count_words(char const *s, char c)
-{
-  size_t  count;
-  int  i;
 
-  i = 0;
-  count = 0;
-  while (s[i] && s[i + 1])
-  {
-    if (s[i] != c && s[i + 1] == c)
-      count++;
-    i++;
-  }
-  count += (s[i] != c && s[0]) ? 1 : 0;
-  return (count);
+static	size_t	count_words(char const *s, char c)
+{
+	size_t		count;
+	int			i;
+
+	i = 0;
+	count = 0;
+	while (s[i] && s[i + 1])
+	{
+		if (s[i] != c && s[i + 1] == c)
+			count++;
+		i++;
+	}
+	count += (s[i] != c && s[0]) ? 1 : 0;
+	return (count);
 }
-/*static size_t word_length(size_t *i, char const *s, char c)
-{
-    size_t  len;
 
-    len = 0;
-    while(s[*i] == c)
-      *(i)++;
-    while (s[*i] != c && s[*i])
-    {
-      len++;
-      *(i)++;
-    }
-    return (len);
-}*/
-static  char **free_words(char **array)
+static	char	**free_words(char **array)
 {
-    size_t      i;
+	size_t	i;
 
-    i = 0;
-    while (array[i])
-    {
-        free(array[i]);
-        i++;
-    }
-    free(array);
-    return (NULL);
+	i = 0;
+	while (array[i])
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
+	return (NULL);
 }
-char  **ft_split(char const *s, char c)
+
+char	**ft_split(char const *s, char c)
 {
-  char  **res;
-  size_t  len;
-  size_t word_len;
-  size_t  i;
-  size_t  j;
+	char		**res;
+	size_t		len;
+	size_t		word_len;
+	size_t		i;
+	size_t		j;
 
-
-  if (!s)
-    return (NULL);
-  len = count_words(s, c);
-
-  if (!(res = (char**)malloc((len + 1) * sizeof(char*))))
-    return (NULL);
-  i = 0;
-  j = 0;
-  while (j < len + (!len))
-   {
-    word_len = 0;
-    while(s[i] == c)
-      i++;
-    while (s[i] != c && s[i])
-    {
-      word_len++;
-      i++;
-    }
-    if (!(res[j] = calloc((word_len + 1), sizeof(char))))
-      return free_words(res);
-    res[j] = ft_memcpy(res[j], s + (i - word_len), word_len);
-    j++;
-  }
-  res[j] = NULL;
-  return (res);
+	if (!s)
+		return (NULL);
+	len = count_words(s, c);
+	if (!(res = (char**)malloc((len + 1) * sizeof(char*))))
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (j < len + (!len))
+	{
+		word_len = 0;
+		while (s[i] == c)
+			i++;
+		while (s[i] != c && s[i])
+		{
+			word_len++;
+			i++;
+		}
+		if (!(res[j] = calloc((word_len + 1), sizeof(char))))
+			return (free_words(res));
+		res[j] = ft_memcpy(res[j], s + (i - word_len), word_len);
+		j++;
+	}
+	res[j] = NULL;
+	return (res);
 }
